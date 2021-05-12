5eee688c4d81ca793bceecf79b37df8baede778a
BumbleBee2811 <dj.dibyajyotidash@gmail.com>
2021-05-12 19:15:20 +0530

    initial changes

---
+++ src/main/java/com/dj/webservices/myrestwebservice/MyRestWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dj.webservices.myrestwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestWebserviceApplication.class, args);
+	}
+
+}
