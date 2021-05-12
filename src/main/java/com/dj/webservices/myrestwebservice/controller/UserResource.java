f4d43023d3e0af59e8a30506d342cef79c1e3883
BumbleBee2811 <dj.dibyajyotidash@gmail.com>
2021-05-13 01:06:25 +0530

    Exception Changes

--- src/main/java/com/dj/webservices/myrestwebservice/controller/UserResource.java
+++ src/main/java/com/dj/webservices/myrestwebservice/controller/UserResource.java
@@ -2,9 +2,13 @@ package com.dj.webservices.myrestwebservice.controller;
 
 import com.dj.webservices.myrestwebservice.bean.User;
 import com.dj.webservices.myrestwebservice.dao.UserDAO;
+import com.dj.webservices.myrestwebservice.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,11 +24,18 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable int id) {
-        return userDAO.findOne(id);
+        User user = userDAO.findOne(id);
+        if (null == user) {
+            throw new UserNotFoundException("User having id = "+id+" was not found. Please verify the id you have entered.");
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public User getUser(@RequestBody User user) {
-        return userDAO.save(user);
+    public ResponseEntity<Object> getUser(@RequestBody User user) {
+        User savedUser = userDAO.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
