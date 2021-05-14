b4e7295820c57df6d6680234590bee2d301d92d1
BumbleBee2811 <dj.dibyajyotidash@gmail.com>
2021-05-14 14:31:18 +0530

    new changes

---
+++ src/test/java/com/dj/webservices/myrestwebservice/controller/UserResourceTest.java
@@ -0,0 +1,24 @@
+package com.dj.webservices.myrestwebservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserResourceTest {
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void testGetUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
