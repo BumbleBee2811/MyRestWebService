d28a02c6dcffd0d2d44120e459de799e9cea2964
BumbleBee2811 <dj.dibyajyotidash@gmail.com>
2021-05-25 00:32:12 +0530

    security changes

---
+++ src/main/java/com/dj/webservices/myrestwebservice/filtering/FilteringController.java
@@ -0,0 +1,25 @@
+package com.dj.webservices.myrestwebservice.filtering;
+
+import com.dj.webservices.myrestwebservice.bean.SomeBean;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+    @GetMapping("/filtering")
+    public MappingJacksonValue retrieveSomeBean() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1");
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("BeanFilter", filter);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+        mappingJacksonValue.setFilters(filterProvider);
+        return mappingJacksonValue;
+    }
+}
